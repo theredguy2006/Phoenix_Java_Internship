89cfc8464e02a0c693742334107f455e1ae4772b
RedGuy2006 <theredguy2006@gmail.com>
2026-01-21 11:09:46 +0530

    Day 1

---
+++ Day-01/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.printf("Hello World ");
+
+        System.out.println(" Using Different kind of escape sequences ");
+        System.out.println(" This one is tab it will leave space like\tthis ");
+        System.out.println(" This one will remove \bspace");
+        System.out.println(" This is Line 1 \n This is new line  ");
+        System.out.println(" This is 'single quote' and \"double quotes \"");
+        System.out.println(" \\ Backslash ");
+        int a = 5; System.out.println(a++ + ++a);
+    }
+}
