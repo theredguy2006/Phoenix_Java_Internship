94cf69bb8db9d45eb4999de6c08c02958fcf8268
RedGuy2006 <theredguy2006@gmail.com>
2026-01-29 15:54:21 +0530

    Mid  Day 7-3nd Approach

--- Day 7/Library.java
+++ Day 7/Library.java
@@ -1,157 +1,284 @@
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Library {
+    static Scanner sc = new Scanner(System.in);
+    static int bID = 0; // And id variable to count and tack book's id.
+    static int pID = 0; // And id variable to count and tack book's id.
+    static int mID = 0; // And id variable to count and tack book's id.
+
     public static void main(String[] args) {
+
         Member[] m = new Member[5];
-        PremiumMember[] pm = new PremiumMember[2];
+        int a = 0;
+        PremiumMember[] pm = new PremiumMember[3];
         Book[] bo = new Book[5];
-        m[0] = new Member("Kaif", 1234);
-        m[1] = new Member("Kaif2", 1234);
-        m[2] = new Member("Kaif3", 1234);
-        bo[0] = new Book("Anxious People", "Freidrick Backman ");
-        bo[1] = new Book("Project Hail Mary ", "Andy Weir ");
-        bo[3] = new Book("The Shining ", "Stephen King ");
+        int bUnavailable[] = new int[Array.getLength(bo)]; // an array to track which books are taken.
+        m[mID++] = new Member("Kaif", 1234);
+        m[mID++] = new Member("Kaif2", 1234);
+        m[mID++] = new Member("Kaif3", 1234);
+        bo[bID] = new Book("Anxious People", "Freidrick Backman ", bID++);
+        bo[bID] = new Book("Project Hail Mary ", "Andy Weir ", bID++);
+        bo[bID] = new Book("The Shining ", "Stephen King ", bID++);
+        bo[bID] = new Book("Normal People  ", "Salley Rooney", bID++);
+        pm[pID++] = new PremiumMember("PKaif2", 1234);
+        pm[pID++] = new PremiumMember("PKaif3", 1234);
+        String name;
+        do {
+            System.out.println("Press 1 to Login ");
+            System.out.println("Press 2 to Regsister ");
+            a = sc.nextInt();
+            switch (a) {
+                case 2:
+                    System.out.println("Enter Your Name ");
+                    name = sc.next();
+                    boolean userExist = false;
+                    for (Member i : m) {
+                        if (i == null)
+                            continue;
+                        if (i.Name.equals(name)) {
+                            userExist = true;
+                            System.out.println("You cannot login with that Username ");
+                            break;
+
+                        }
+                    }
+                    for (PremiumMember i : pm) {
+                        if (i == null)
+                            continue;
+                        if (i.Name.equals(name)) {
+                            userExist = true;
+                            System.out.println("You cannot login with that Username ");
+                            break;
+                        }
+
+                    }
+                    if (!userExist) {
+                        System.out.println("Enter Your Password(Numeric Only)");
+                        int b = sc.nextInt();
+                        System.out.println(
+                                "Do you want to be a premium user ? \n Press 1.) For Yes \n Press Other Number for No ");
+                        int c = sc.nextInt();
+                        if (c == 1) {
+                            pm[pID++] = new PremiumMember(name, b);
+                        } else {
+                            m[mID++] = new Member(name, b);
+
+                        }
+                    }
+                    break;
+                case 1:
+                    System.out.println("Enter Your Name  ");
+                    name = sc.next();
+                    System.out.println("Enter Your Password  ");
+                    int pwd = sc.nextInt();
+                    for (Member i : m) {
+                        if (i == null)
+                            continue;
+                        if (i.Name.equals(name) && i.pwd == pwd) {
+                            System.out.println("Login Sucessful ");
+                            i.Menu(bo);
+
+                        }
+                    }
+                    for (PremiumMember i : pm) {
+                        if (i == null)
+                            continue;
+                        if (i.Name.equals(name) && i.pwd == pwd) {
+                            System.out.println("Login Sucessful ");
+                            i.Menu(bo);
+                        }
+
+                    }
+
+                    break;
+                default:
+                    System.out.println("Non Premium Members ");
+                    for (Member i : m) {
+                        if (i == null)
+                            continue;
+
+                        System.out.println("Name--> " + i.Name);
+                        System.out.println("PWD--> " + i.pwd);
+                        System.out.println("---------");
+                    }
+                    for (PremiumMember i : pm) {
+                        if (i == null)
+                            continue;
+
+                        System.out.println("Name-->" + i.Name);
+                        System.out.println("PWD-->" + i.pwd);
+                        System.out.println("---------");
+                    }
+                    for (Book i : bo) {
+                        if (i == null)
+                            continue;
+
+                        System.out.println("Name--> " + i.bookName);
+                        System.out.println("Author Name --> " + i.authorName);
+                        System.out.println("Book Id --> " + i.bookID);
+                        System.out.println("---------");
+                    }
+                    break;
+            }
+        } while (a != 3);
 
     }
 
 }
 
-interface searchable {
-    void menu(Member m);
-
-    void login(Member m);
-
-    void register(Member m);
-
-    void borrowBook();
-
-    void donateBook();
-
-    void checkBooks();
-
-    void checkAvailableBooks();
-}
-
-interface borrowable {
-    void bookBorrowed();
-
-    void bookDonated();
-
-    void bookTracker();
-}
-
-class PremiumMember implements searchable {
-    Scanner sc = new Scanner(System.in);
-    int id;
+class PremiumMember {
     String Name;
+    Book booksTaken[] = new Book[3];
+    int btCounter = 0;
     int pwd;
-    boolean isPremium;
+    Book[] bPM;
+    Scanner sc = new Scanner(System.in);
+    Library lm = new Library();
 
     public PremiumMember(String name, int pwd) {
         this.Name = name;
         this.pwd = pwd;
-        this.isPremium = true;
     }
 
-    @Override
-    public void login(Member m) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    void Menu(Book[] b) {
+        int key;
+        this.bPM = b;
+        do {
+            System.out.println("Press 1.) To See Book you taken.  ");
+            System.out.println("Press 2.) To See All books ");
+            System.out.println("Press 3.) To Donate a Book  ");
+            System.out.println("Press 4.) To Borrow a Book  ");
+            System.out.println("Press 5.) To Logout ");
+            key = sc.nextInt();
+            switch (key) {
+                case 1:
+                    booksTaken();
+                    break;
+                case 2:
+                    checkAvailableBooks();
+                    break;
+                case 3:
+                    donateBook();
+                    break;
+                case 4:
+                    takeBook();
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+
+        } while (key != 5);
     }
 
-    @Override
-    public void borrowBook() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    void checkAvailableBooks() {
+        for (Book i : bPM) {
+            if (i == null)
+                continue;
+            ;
+            System.out.println("Name--> " + i.bookName);
+            System.out.println("Author Name --> " + i.authorName);
+            System.out.println("Book Id --> " + i.bookID);
+            System.out.println("---------");
+            if (i.isBorrowed) {
+                System.out.println("Book is Taken By " + i.borrowedBy);
+            } else {
+                System.out.println("Available");
+            }
+        }
+
     }
 
-    @Override
-    public void donateBook() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    void booksTaken() {
+        boolean bookTaken = false;
+        for (Book i : booksTaken) {
+            if (i == null) {
+                continue;
+            } else {
+                System.out.println("Name--> " + i.bookName);
+                System.out.println("Author Name --> " + i.authorName);
+                System.out.println("Book Id --> " + i.bookID);
+                System.out.println("---------");
+
+            }
+
+        }
+
     }
 
-    @Override
-    public void checkBooks() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+    void takeBook() {
+        if (btCounter < booksTaken.length) {
 
-    @Override
-    public void checkAvailableBooks() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+            for (Book i : bPM) {
+                if (i == null || i.isBorrowed)
+                    continue;
 
-    @Override
-    public void menu(Member m) {
-        System.out.println("Welcome to Red's Library ");
-        System.out.println("Press 1.) To Login ");
-        System.out.println("Press 2.) To Register ");
-        System.out.println("-------------------------------");
-        System.out.println("Debuggine Portion Comment out later");
-        System.out.println("Checking all the Books--->  ");
-        System.out.println("Checking all the Members--->  ");
-        System.out.println("Checking all the PremiumMembers--->  ");
-        int key = sc.nextInt();
-        switch (key) {
-            case 1:
-                this.login(m);
+                System.out.println("Name--> " + i.bookName);
+                System.out.println("Author Name --> " + i.authorName);
+                System.out.println("Book Id --> " + i.bookID);
+                System.out.println("---------");
+            }
 
-                break;
-            case 2:
-                this.register(m);
-                break;
+            System.out.println("Enter Id of the Book you want ");
+            int boName = sc.nextInt();
+            boolean s = false;
 
-            default:
-                System.out.println("Work in Progress ");
-                break;
+            for (Book i : bPM) {
+                if (i == null || i.isBorrowed)
+                    continue;
+
+                if (boName == i.bookID) {
+                    booksTaken[btCounter] = new Book(i.bookName, i.authorName, i.bookID);
+                    booksTaken[btCounter].borrowedBy = this.Name;
+                    i.isBorrowed = true;
+                    btCounter++;
+                    s = true;
+                    break;
+                }
+            }
+
+            if (s)
+                System.out.println("Book Issued Successfully ");
+            else
+                System.out.println("Unfortunately there was some error , please try again ");
+
+        } else {
+            System.out.println("You have exceeded the no of books you can take please return one of them ");
         }
     }
 
-    @Override
-    public void register(Member m) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    void donateBook() {
+
     }
 
 }
 
 class Member extends PremiumMember {
-    // Scanner sc = new Scanner(System.in);
-    // int id;
-    // String Name;
-    // int pwd;
-    // boolean isPremium;
+    String Name;
+    int bookTaken;
+    int pwd;
+
     public Member(String name, int pwd) {
         super(name, pwd);
         this.Name = name;
         this.pwd = pwd;
-        this.isPremium = false;
     }
 
 }
 
-class Book implements borrowable {
+class Book {
     String bookName;
     String authorName;
     int bookID;
     boolean isBorrowed;
-    int borrowedBy;
+    String borrowedBy;
 
-    public Book(String bookName, String authorName) {
+    public Book(String bookName, String authorName, int bookID) {
         this.bookName = bookName;
         this.authorName = authorName;
-    }
-
-    @Override
-    public void bookBorrowed() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void bookDonated() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void bookTracker() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.bookID = bookID;
+        this.isBorrowed = false;
     }
 
 }
---
+++ Day 7/test.java
@@ -0,0 +1,166 @@
+// import java.util.Scanner;
+
+// public class Library {
+//     public static void main(String[] args) {
+//         Member[] m = new Member[5];
+//         PremiumMember[] pm = new PremiumMember[2];
+//         Book[] bo = new Book[5];
+//         m[0] = new Member("Kaif", 1234);
+//         m[1] = new Member("Kaif2", 1234);
+//         m[2] = new Member("Kaif3", 1234);
+//         bo[0] = new Book("Anxious People", "Freidrick Backman ");
+//         bo[1] = new Book("Project Hail Mary ", "Andy Weir ");
+//         bo[3] = new Book("The Shining ", "Stephen King ");
+
+//     }
+
+// }
+
+// interface searchable {
+//     void menu(Member[] m);
+
+//     void login(Member m);
+
+//     void register(Member m);
+
+//     void borrowBook();
+
+//     void donateBook();
+
+//     void checkBooks();
+
+//     void checkAvailableBooks();
+// }
+
+// interface borrowable {
+//     void bookBorrowed();
+
+//     void bookDonated();
+
+//     void bookTracker();
+// }
+
+// class PremiumMember implements searchable {
+//     Scanner sc = new Scanner(System.in);
+//     int id;
+//     String Name;
+//     int pwd;
+//     boolean isPremium;
+
+//     public PremiumMember(String name, int pwd) {
+//         this.Name = name;
+//         this.pwd = pwd;
+//         this.isPremium = true;
+//     }
+
+//     @Override
+//     public void login(Member m) {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+
+//     @Override
+//     public void borrowBook() {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+
+//     @Override
+//     public void donateBook() {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+
+//     @Override
+//     public void checkBooks() {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+
+//     @Override
+//     public void checkAvailableBooks() {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+
+//     @Override
+//     public void menu(Member[] m) {
+//         System.out.println("Welcome to Red's Library ");
+//         System.out.println("Press 1.) To Login ");
+//         System.out.println("Press 2.) To Register ");
+//         System.out.println("-------------------------------");
+//         System.out.println("Debuggine Portion Comment out later");
+//         System.out.println("Checking all the Books--->  ");
+//         System.out.println("Checking all the Members--->  ");
+//         System.out.println("Checking all the PremiumMembers--->  ");
+//         int key = sc.nextInt();
+//         switch (key) {
+//             case 1:
+//                 System.out.println("Enter Your Name  ");
+//                 String name = sc.next();
+//                 System.out.println("Enter Your Password  ");
+//                 int pwd1 = sc.nextInt();
+//                 for (Member i : m) {
+//                     if (i == null)
+//                         continue;
+//                     if (i.Name.equals(name) && i.pwd == pwd1) {
+//                         System.out.println("Login Sucessful ");
+//                     }
+//                 }
+
+//                 break;
+//             case 2:
+//                 this.register(m[0]);
+//                 break;
+
+//             default:
+//                 System.out.println("Work in Progress ");
+//                 break;
+//         }
+//     }
+
+//     @Override
+//     public void register(Member m) {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+
+// }
+
+// class Member extends PremiumMember {
+//     // Scanner sc = new Scanner(System.in);
+//     // int id;
+//     // String Name;
+//     // int pwd;
+//     // boolean isPremium;
+//     public Member(String name, int pwd) {
+//         super(name, pwd);
+//         this.Name = name;
+//         this.pwd = pwd;
+//         this.isPremium = false;
+//     }
+
+// }
+
+// class Book implements borrowable {
+//     String bookName;
+//     String authorName;
+//     int bookID;
+//     boolean isBorrowed;
+//     String borrowedBy;
+
+//     public Book(String bookName, String authorName) {
+//         this.bookName = bookName;
+//         this.authorName = authorName;
+//     }
+
+//     @Override
+//     public void bookBorrowed() {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+
+//     @Override
+//     public void bookDonated() {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+
+//     @Override
+//     public void bookTracker() {
+//         throw new UnsupportedOperationException("Not supported yet.");
+//     }
+
+// }
